314404dcea16698678a57dad708104cb2a803754
robertmirro <rmirro@gmail.com>
2014-11-05 20:32:12 -0500

    more tests

--- test/test.js
+++ test/test.js
@@ -1,25 +1,52 @@
 var expect = require('chai').expect ,
     LinkedList = require('../src/ds_linked_list') ,
-    util = require('util');
+    util = require('util') ,
+    linkedList;
 
 //console.log( util.inspect( LinkedList('hola') , {showHidden: true, depth: null, colors: true} ) );
 
 describe( 'LinkedList' , function () {
-    var linkedList;
- 
-    before(function () {
-        linkedList = new LinkedList();
+
+    describe( 'factory constructor' , function () {
+        it( 'should be a function with params' , function () {
+            expect( LinkedList ).to.be.a( 'function' );
+            expect( LinkedList ).to.have.length( 2 );
+        });
     });
- 
-    describe( 'constructor' , function() {
-        it( 'should have properties' , function () {
-            expect( linkedList ).to.have.property( 'size' );
-            expect( linkedList ).to.have.property( 'uh oh' );
+        
+    describe( 'empty linked list' , function() {
+        before( function () {
+            linkedList = new LinkedList();
+        });
+        
+        it( 'should be an object with methods indicating an empty list' , function () {
+            expect( linkedList ).to.be.an( 'object' );
+            expect( linkedList ).to.have.property( 'size' , 0 );
+            expect( linkedList ).to.have.property( 'isEmpty' , true );
+            expect( linkedList.first() ).to.equal( null );
+            expect( linkedList.last() ).to.equal( null );
+            expect( linkedList.current() ).to.equal( null );
         });
     });
 });
 
-
+//    'size'          : { 'get'   : size , 'enumerable' : true , 'configurable' : false } ,
+//    'isEmpty'       : { 'get'   : isEmpty , 'enumerable' : true , 'configurable' : false } ,
+//    'empty'         : { 'value' : empty , 'enumerable' : true , 'configurable' : false , 'writable' : false } ,
+//    'first'         : { 'value' : first , 'enumerable' : true , 'configurable' : false , 'writable' : false } ,
+//    'last'          : { 'value' : last , 'enumerable' : true , 'configurable' : false , 'writable' : false } ,
+//    'current'       : { 'value' : current , 'enumerable' : true , 'configurable' : false , 'writable' : false } ,
+//    'next'          : { 'value' : next , 'enumerable' : true , 'configurable' : false , 'writable' : false } ,
+//    'get'           : { 'value' : get , 'enumerable' : true , 'configurable' : false , 'writable' : false } ,
+//    'getAt'         : { 'value' : getAt , 'enumerable' : true , 'configurable' : false , 'writable' : false } ,
+//    'add'           : { 'value' : addFirst , 'enumerable' : true , 'configurable' : false , 'writable' : false } ,
+//    'addLast'       : { 'value' : addLast , 'enumerable' : true , 'configurable' : false , 'writable' : false } ,
+//    'addAt'         : { 'value' : addAt , 'enumerable' : true , 'configurable' : false , 'writable' : false } ,
+//    'remove'        : { 'value' : remove , 'enumerable' : true , 'configurable' : false , 'writable' : false } ,
+//    'removeAt'      : { 'value' : removeAt , 'enumerable' : true , 'configurable' : false , 'writable' : false } ,
+//    'removeCurrent' : { 'value' : removeCurrent , 'enumerable' : true , 'configurable' : false , 'writable' : false } ,
+//    'toArray'       : { 'value' : toArray , 'enumerable' : true , 'configurable' : false , 'writable' : false } ,
+//    'forEach'       : { 'value' : forEachNode , 'enumerable' : true , 'configurable' : false , 'writable' : false }
 
 //var assert = require('assert');
 //
