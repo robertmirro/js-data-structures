56bac315bbb857fed86fceaf3af0adcdb6194a49
robertmirro <rmirro@gmail.com>
2014-11-05 20:08:31 -0500

    chai expect

--- test/test.js
+++ test/test.js
@@ -1,10 +1,33 @@
-var assert = require('assert');
+var expect = require('chai').expect ,
+    LinkedList = require('../src/ds_linked_list') ,
+    util = require('util');
 
-describe('Array', function(){
-    describe('#indexOf()', function(){
-        it('should return -1 when the value is not present', function(){
-            assert.equal(-1, [1,2,3].indexOf(5));
-            assert.equal(-1, [1,2,3].indexOf(0));
-            })
-    })
-})
+//console.log( util.inspect( LinkedList('hola') , {showHidden: true, depth: null, colors: true} ) );
+
+describe( 'LinkedList' , function () {
+    var linkedList;
+ 
+    before(function () {
+        linkedList = new LinkedList();
+    });
+ 
+    describe( 'constructor' , function() {
+        it( 'should have properties' , function () {
+            expect( linkedList ).to.have.property( 'size' );
+            expect( linkedList ).to.have.property( 'uh oh' );
+        });
+    });
+});
+
+
+
+//var assert = require('assert');
+//
+//describe('Array', function(){
+//    describe('#indexOf()', function(){
+//        it('should return -1 when the value is not present', function(){
+//            assert.equal(-1, [1,2,3].indexOf(5));
+//            assert.equal(-1, [1,2,3].indexOf(0));
+//            })
+//    })
+//})
